f773f640dafaaa5483a490f578a4d25b1733c6e4
Diego Soler <dpsoler09@gmail.com>
2023-09-21 08:50:10 -0300

    fix issue

--- fix_issue.py
+++ fix_issue.py
@@ -1,5 +1,18 @@
 import requests
 import os
+from tqdm import tqdm
+
+def extract_repo_names_from_file(file_path):
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+
+    repo_names = []
+    for line in lines:
+        # Split the URL by '/' and get the last part as the repo name
+        repo_name = line.strip().split('/')[-1]
+        repo_names.append(repo_name)
+
+    return repo_names
 
 # Step 1: Get GitHub credentials from a file
 with open('credentials.txt', 'r') as f:
@@ -10,37 +23,38 @@ headers = {
     'Accept': 'application/vnd.github.v3+json'
 }
 
-# Step 2: Given a repo link, list all open issues
-# repo_link = input("Enter the GitHub repo link (format: owner/repo): ")
-repo_link = "insper-education/issue_generator"
-response = requests.get(f'https://api.github.com/repos/{repo_link}/issues', headers=headers, params={'state': 'open'})
+# Extract repository names from the file
+repo_names = extract_repo_names_from_file("repositories.txt")
 
-if response.status_code != 200:
-    print("Error fetching issues:", response.json().get('message'))
-    exit()
+for repo_name in tqdm(repo_names):
+    # Step 2: For each repo, list all open issues
+    response = requests.get(f'https://api.github.com/repos/insper-classroom/{repo_name}/issues', headers=headers, params={'state': 'open'})
 
-issues = response.json()
+    if response.status_code != 200:
+        print(f"Error fetching issues for {repo_name}:", response.json().get('message'))
+        continue
 
-# Step 3: Edit the issues that have the same name as the issues in a directory
-issue_dir = 'issues'
-for issue in issues:
-    issue_title = issue['title'] + '.md'
-    issue_file_path = os.path.join(issue_dir, issue_title)
-    
-    if os.path.exists(issue_file_path):
-        print(f"Found issue: {issue_title}")
-        with open(issue_file_path, 'r') as f:
-            new_body = f.read()
+    issues = response.json()
+
+    # Step 3: Edit the issues that have the same name as the issues in a directory
+    issue_dir = 'issues'
+    for issue in issues:
+        issue_title = issue['title'] + '.md'
+        issue_file_path = os.path.join(issue_dir, issue_title)
         
-        data = {
-            'body': new_body
-        }
-        
-        response = requests.patch(f'https://api.github.com/repos/{repo_link}/issues/{issue["number"]}', headers=headers, json=data)
-        
-        if response.status_code == 200:
-            print(f"Updated issue: {issue_title}")
-        else:
-            print(f"Failed to update issue: {issue_title}. Error: {response.json().get('message')}")
+        if os.path.exists(issue_file_path):
+            print(f"Found issue: {issue_title} in {repo_name}")
+            with open(issue_file_path, 'r') as f:
+                new_body = f.read()
+            
+            data = {
+                'body': new_body
+            }
+            response = requests.patch(f'https://api.github.com/repos/insper-classroom/{repo_name}/issues/{issue["number"]}', headers=headers, json=data)
+            
+            if response.status_code == 200:
+                print(f"Updated issue: {issue_title} in {repo_name}")
+            else:
+                print(f"Failed to update issue: {issue_title} in {repo_name}. Error: {response.json().get('message')}")
 
 print("Done!")
